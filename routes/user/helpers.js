e0ffb14d847a2eae0d326519def852bf7c3e4152
Abhishek Singh <abhiwebdev2718@gmail.com>
2025-03-02 14:54:55 +0530

    pagination in user list

--- routes/user/helpers.js
+++ routes/user/helpers.js
@@ -1,37 +1,37 @@
 const User = require('../../modals/user');
-exports.getUser = async (search) => {
+exports.getUser = async (pageSize, skip) => {
     // return User.findOne(
     //     {
     //     $text: {
     //         $search: search
     //     }
     // })
-    // return User.aggregate([
-    //     {
-    //         $facet: {
-    //             items: [{
-    //                 $skip: skip
-    //             },
-    //             {
-    //                 $limit: pageSize
-    //             }
-    //             ]
-    //         }
-    //     },
-    //     {
-    //         $addFields: {
-    //             page,
-    //             pageSize
-    //         }
-    //     },
-    //     {
-    //         $project: {
-    //             items: 1,
-    //             page: 1,
-    //             pageSize: 1
-    //         }
-    //     }
-    // ]);
+    return User.aggregate([
+        {
+            $facet: {
+                items: [{
+                    $skip: skip
+                },
+                {
+                    $limit: pageSize
+                }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                page,
+                pageSize
+            }
+        },
+        {
+            $project: {
+                items: 1,
+                page: 1,
+                pageSize: 1
+            }
+        }
+    ]);
     return User.findById("67aeff3b76b42cf1a6bb0a29");
 }
 
--- routes/user/routes.js
+++ routes/user/routes.js
@@ -1,18 +1,15 @@
 const userHelpers = require("./helpers");
 exports.get = async (req, res, next) => {
-  // const pageSize = req.query?.pageSize ? Number(req.query.pageSize) : 10;
-  // const page = req.query?.page ? Number(req.query.page) : 1;
-  // const skip = (page - 1) * page;
-  const {search} = req.query
+  const pageSize = req.query?.pageSize ? Number(req.query.pageSize) : 10;
+  const page = req.query?.page ? Number(req.query.page) : 1;
+  const skip = (page - 1) * page;
   try {
-    const data = await userHelpers.getUser(search);
-    //console.log("Data is here ---->",data);
+    const data = await userHelpers.getUser(pageSize, skip);
     res.status(200).json({
       success: true,
-      data
-      // users: data[0].items,
-      // page: data[0].page,
-      // pageSize: data[0].pageSize
+      users: data[0].items,
+      page: data[0].page,
+      pageSize: data[0].pageSize
     });
   } catch (error) {
     next(error);
